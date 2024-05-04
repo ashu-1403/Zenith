3991cf67070a5552af29540d18011c580732d32a
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2024-05-04 22:07:28 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import path from "path"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+
+import connectToMongoDb from "./DB/connecttoMongoDb.js";
+import {server,app} from "./socket/socket.js";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("",(req,res) =>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+
+server.listen(PORT, () => {
+  connectToMongoDb();
+  console.log(`Server is running on port ${PORT}`);
+});
